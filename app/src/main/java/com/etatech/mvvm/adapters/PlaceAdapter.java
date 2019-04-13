da09a548e3e3782e2d3fbb2c124b30266e45e85b
Fady <fady.fouad.a@gmail.com>
2019-04-13 22:58:29 +0200

    Create Adapter Class

--- app/src/main/java/com/etatech/mvvm/adapters/PlaceAdapter.java
+++ app/src/main/java/com/etatech/mvvm/adapters/PlaceAdapter.java
@@ -1,4 +1,65 @@
 package com.etatech.mvvm.adapters;
 
-public class PlaceAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.etatech.mvvm.R;
+import com.etatech.mvvm.model.Place;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHolder> {
+
+    private List<Place>places = new ArrayList<>();
+    private Context context ;
+
+    public PlaceAdapter(List<Place> places, Context context) {
+        this.places = places;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public PlaceViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_place_item,viewGroup,false);
+        return new PlaceViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlaceViewHolder placeViewHolder, int i) {
+
+        placeViewHolder.image_desc.setText(places.get(i).getTitle());
+
+        Glide.with(context)
+                .load(places.get(i).getImgUrl())
+                .into(placeViewHolder.imageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return places.size();
+    }
+
+    public class PlaceViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView imageView ;
+        TextView image_desc ;
+
+        public PlaceViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.circle_image);
+            image_desc = itemView.findViewById(R.id.image_desc);
+        }
+    }
 }
